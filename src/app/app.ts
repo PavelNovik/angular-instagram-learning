7faafa32d453ba258969a1ab6f67fa6c5d612ba8
Pavel Novik <brn2befree@gmail.com>
2025-12-20 18:14:14 +0300

    create some components

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {Test1Component} from './test1/test1.component';
 import {Test2} from './test2/test2';
 import {Test3} from './test3/test3';
+import {Test4} from './user/components/test4/test4';
+import {Test5} from './test5/test5';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Test1Component, Test2, Test3],
+  imports: [RouterOutlet, Test1Component, Test2, Test3, Test4, Test5],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/test5/test5.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inst-test5',
+  imports: [],
+  template: `<p class="par">test 5</p>
+  <h2 class="title">test5</h2>
+  `,
+  styles: [`
+  .par {
+    color: aqua;
+  }
+  .title {
+    color: darkolivegreen;
+  }
+  `],
+})
+export class Test5 {
+
+}

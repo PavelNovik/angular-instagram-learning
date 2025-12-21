c08a7748bbc4fa98d4c82f9f82db14558a679e5d
Pavel Novik <brn2befree@gmail.com>
2025-12-21 21:18:45 +0300

    add ngFor / ngForOf directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Child } from '../child/child';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'inst-root',
   standalone: true,
-  imports: [FormsModule, Child],
+  imports: [FormsModule, Child, NgForOf],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
 export class App {
+  arr: string[] = ['asda', 'dasdad', 'adsadd'];
   name?: string;
   appTitle = 'Instagram';
 

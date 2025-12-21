4b9650d495eba130f69dc1c40bc383b879b3fc59
Pavel Novik <brn2befree@gmail.com>
2025-12-21 17:58:47 +0300

    continued to learn basis

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Test1Component} from './test1/test1.component';
-import {Test2} from './test2/test2';
-import {Test3} from './test3/test3';
-import {Test4} from './user/components/test4/test4';
-import {Test5} from './test5/test5';
+
+type IUser = {
+  age: number;
+  name: string;
+};
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Test1Component, Test2, Test3, Test4, Test5],
+  selector: 'inst-root',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-instagram');
+  appTitle = 'Instagram';
+  user: IUser = {
+    age: 32,
+    name: 'John Doe',
+  };
 }

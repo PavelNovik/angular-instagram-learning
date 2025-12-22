e77f84740cac5c167537c37162356c69d8ef07d7
Pavel Novik <brn2befree@gmail.com>
2025-12-22 09:27:46 +0300

    add ngIf else directive in proj

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Child } from '../child/child';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'inst-root',
   standalone: true,
-  imports: [FormsModule, Child, NgForOf],
+  imports: [FormsModule, Child, NgForOf, NgIf],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
@@ -14,10 +14,14 @@ export class App {
   arr: string[] = ['asda', 'dasdad', 'adsadd'];
   name?: string;
   appTitle = 'Instagram';
+  isLoading = signal(true);
 
   text = 'start value';
 
   getName(name: string): void {
     this.name = name;
+  }
+  constructor() {
+    setTimeout(() => this.isLoading.set(false), 3000);
   }
 }

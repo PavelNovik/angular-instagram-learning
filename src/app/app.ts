4ed305533f3649d2069ea3bd3f7f5abacf039dff
Pavel Novik <brn2befree@gmail.com>
2025-12-24 15:38:24 +0300

    add slice pipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,18 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Child } from '../child/child';
 import { Fruit } from '../fruit/fruit';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'inst-root',
   standalone: true,
-  imports: [FormsModule, Child, Fruit],
+  imports: [FormsModule, Child, Fruit, SlicePipe],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
 export class App {
+  lorem =
+    ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cupiditate error et iusto quas quis rerum veniam? Commodi corporis, dolorem, ducimus facilis impedit libero, minima modi perferendis placeat temporibus velit!';
   value = '';
   arr: string[] = ['One', 'Two', 'Three'];
   name?: string;

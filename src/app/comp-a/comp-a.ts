77745649b6dc739328a372300b10cb5060d1b4dd
Pavel Novik <brn2befree@gmail.com>
2025-12-26 14:50:05 +0300

    remove subscription from components and add observer

--- src/app/comp-a/comp-a.ts
+++ src/app/comp-a/comp-a.ts
@@ -1,24 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ValueService } from '../services/value.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'inst-comp-a',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './comp-a.html',
   styleUrl: './comp-a.scss',
 })
 export class CompA implements OnInit {
   private valueService = inject(ValueService);
-  value = 0;
+  value$ = new Observable();
   protected addValueHandler(): void {
     this.valueService.add();
     console.log(this.valueService.value$);
   }
 
   ngOnInit(): void {
-    this.valueService.value$.subscribe((value) => {
-      this.value = value;
-    });
+    // this.valueService.value$.subscribe((value) => {
+    //   this.value = value;
+    // });
+    this.value$ = this.valueService.value$;
   }
 }
--- src/app/comp-b/comp-b.ts
+++ src/app/comp-b/comp-b.ts
@@ -1,24 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ValueService } from '../services/value.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'inst-comp-b',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './comp-b.html',
   styleUrl: './comp-b.scss',
 })
 export class CompB implements OnInit {
   private valueService = inject(ValueService);
-  value = 0;
+  value$ = new Observable();
   protected decValueHandler(): void {
     this.valueService.dec();
     console.log(this.valueService.value$);
   }
 
   ngOnInit(): void {
-    this.valueService.value$.subscribe((value) => {
-      this.value = value;
-    });
+    // this.valueService.value$.subscribe((value) => {
+    //   this.value = value;
+    // });
+    this.value$ = this.valueService.value$;
   }
 }

e1628ee23ed9a6fdb2ec473155c14cb8e929b3ab
Pavel Novik <brn2befree@gmail.com>
2025-12-29 01:22:35 +0300

    add some validators

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { email } from '@angular/forms/signals';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'inst-login',
@@ -10,12 +9,14 @@ import { email } from '@angular/forms/signals';
 })
 export class Login {
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', Validators.required),
   });
   onSubmit(): void {
     console.log(JSON.stringify(this.loginForm.value));
   }
 
-  protected readonly email = email;
+  get email() {
+    return this.loginForm.get('email');
+  }
 }

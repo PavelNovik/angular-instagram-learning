b953a11ba24386c025e90caa8fd1418b90635907
Pavel Novik <brn2befree@gmail.com>
2026-01-03 22:49:16 +0300

    add back function

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ProfileService, ProfileT } from '../../services/profile';
 import { map, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'inst-profile',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
@@ -14,15 +14,19 @@ export class Profile implements OnInit {
   profile$!: Observable<ProfileT>;
   private route = inject(ActivatedRoute);
   private getUserProfiles = inject(ProfileService);
+  private router = inject(Router);
 
   ngOnInit(): void {
     const userId = this.route.snapshot.paramMap.get('userId');
 
     this.profile$ = this.getUserProfiles.getUserProfile(userId!);
 
-    console.log(this.profile$);
-    console.log(userId);
+    // console.log(userId);
   }
 
   protected readonly map = map;
+
+  protected backToUsersHandler(): void {
+    this.router.navigate(['/users']);
+  }
 }
